bb3c554364b1d28972b305a32c5bc07773b39a4a
Peter Patoschka <patoschkapeter@gmail.com>
2024-10-02 16:38:26 +0200

    altered installer to set alias for google-chrome binary in mac-app package so we can use 'google-chrome' as a command in further scripting

--- install.sh
+++ install.sh
@@ -1,25 +1,55 @@
 #!/bin/sh
 
+# Detect the operating system
+OS="$(uname -s)"
+
 # Determine the directory of the script
 DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 # Define the source command
 source_cmd="source \"$DIR/loader.sh\""
 
+# Define Google Chrome alias for mac
+alias_google_chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+
 # Initialize the file variable based on the current shell
 file=""
-case "$SHELL" in
-  *bash)
-    file="$HOME/.bashrc"
-    ;;
-  *zsh)
-    file="$HOME/.zshrc"
-    ;;
-  *)
-    echo "Unknown shell. Please report an issue!"
-    exit 1
-    ;;
-esac
+
+
+if [ "$OS" = "Darwin" ]; then
+    # Check for Homebrew
+    if ! command -v brew >/dev/null 2>&1; then
+        echo "Homebrew not found. Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew update
+    brew install \
+        ca-certificates \
+        curl \
+        gnupg \
+        wget \
+        git \
+        jq \
+        neovim \
+        nss
+
+    file="$HOME/.zprofile"
+else
+  case "$SHELL" in
+    *bash)
+      file="$HOME/.bashrc"
+      ;;
+    *zsh)
+      file="$HOME/.zshrc"
+      ;;
+    *)
+      echo "Unknown shell. Please report an issue!"
+      exit 1
+      ;;
+  esac
+fi
+
 
 # Check if the shell configuration file exists
 if [ ! -f "$file" ]; then
@@ -33,6 +63,17 @@ if grep -qF "$source_cmd" "$file"; then
 else
   echo "Registering loader in $file"
   echo "$source_cmd" >> "$file"
+fi
+
+if [ "$OS" = "Darwin" ]; then
+  if grep -qF "$alias_google_chrome" "$file"; then
+    echo "Alias already set."
+  else
+    echo "Setting alias for Google-Chrome Mac App to chrome binary in $file"
+    echo "$alias_google_chrome" >> "$file"
+  fi
+else
+  echo "not a mac. not setting an alias to App package"
 fi
 
 # Run the setup script if --setup argument is passed
